98a3cd0ce4168656c21c6a6231f73e56be7a24f3
6dQ2fX5g <emmagatzematge70@gmail.com>
2025-10-22 11:57:03 +0200

    Update GitHub repository URL in install script

--- install.sh
+++ install.sh
@@ -2,23 +2,18 @@
 
 echo "=== Wifi Radar installer for Termux ==="
 
-# Actualitzar paquets
 echo "[1/5] Updating packages..."
 pkg update -y && pkg upgrade -y
 
-# Instal·lar dependències
 echo "[2/5] Downloading dependencies (python, termux-api, git)..."
 pkg install python termux-api git -y
 
-# Instal·lar Flask
 echo "[3/5] installing Flask..."
 pip install flask
 
-# Clonar el repositori
 echo "[4/5] Downloading code from Github..."
-git clone https://github.com/el-teu-nom/termux-wifi-radar.git ~/termux-wifi-radar
+git clone https://github.com/6dQ2fX5g/termux-wifi-radar.git ~/termux-wifi-radar
 
-# Crear un àlies per executar fàcilment
 echo "[5/5] Creating direct access 'wifi-radar'..."
 echo "alias wifi-radar='python ~/termux-wifi-radar/wifi_radar.py'" >> ~/.bashrc
 source ~/.bashrc
